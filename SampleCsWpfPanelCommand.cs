4997bb8d68a9efe3f89f6000359c294a6f66cdf5
dalefugier <dale@mcneel.com>
2014-06-23 11:11:05 -0700

    Initial commit

---
+++ SampleCsWpfPanelCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace SampleCsWpfPanel
+{
+  [System.Runtime.InteropServices.Guid("9c4f3dc1-4cda-4678-b5b4-050d87995f60")]
+  public class SampleCsWpfPanelCommand : Command
+  {
+    /// <summary>
+    /// Public constructor
+    /// </summary>
+    public SampleCsWpfPanelCommand()
+    {
+    }
+
+    /// <summary>
+    /// The command name as it appears on the Rhino command line.
+    /// </summary>
+    public override string EnglishName
+    {
+      get { return "SampleCsWpfPanelCommand"; }
+    }
+
+    /// <summary>
+    /// Called by Rhino when the user wants to run this command.
+    /// </summary>
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      return Result.Success;
+    }
+  }
+}
---
+++ SampleCsWpfPanelPlugIn.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace SampleCsWpfPanel
+{
+  ///<summary>
+  /// Every RhinoCommon plug-in must have one and only one Rhino.PlugIns.PlugIn
+  /// inherited class. DO NOT create instances of this class yourself. It is the
+  /// responsibility of Rhino to create an instance of this class.
+  ///</summary>
+  public class SampleCsWpfPanelPlugIn : Rhino.PlugIns.PlugIn
+  {
+    public SampleCsWpfPanelPlugIn()
+    {
+      Instance = this;
+    }
+
+    ///<summary>Gets the only instance of the SampleCsWpfPanelPlugIn plug-in.</summary>
+    public static SampleCsWpfPanelPlugIn Instance
+    {
+      get;
+      private set;
+    }
+
+    // You can override methods here to change the plug-in behavior on
+    // loading and shut down, add options pages to the Rhino _Option command
+    // and mantain plug-in wide options in a document.
+
+    /// <summary>
+    /// Is called when the plug-in is being loaded.
+    /// </summary>
+    protected override Rhino.PlugIns.LoadReturnCode OnLoad(ref string errorMessage)
+    {
+      System.Type panel_type = typeof(SampleCsWpfPanelHost);
+      Rhino.UI.Panels.RegisterPanel(this, panel_type, "Sample", SampleCsWpfPanel.Properties.Resources.Main);
+
+      return Rhino.PlugIns.LoadReturnCode.Success;
+    }
+  }
+
+  /// <summary>
+  /// Rhino framework requires a System.Windows.Forms.IWin32Window derived object for a panel.
+  /// </summary>
+  [System.Runtime.InteropServices.Guid("C29BC573-2D37-4E84-8357-3830CD50BD21")]
+  public class SampleCsWpfPanelHost : RhinoWindows.Controls.WpfElementHost
+  {
+    public SampleCsWpfPanelHost()
+      : base(new SampleCsWpfPanelUserControl(), null) // No view model (for this example)
+    {
+    }
+  }
+}
